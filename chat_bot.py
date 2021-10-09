db78aa9f36e3cebcec740bc78081eb57c4df63c1
Nishith A <nishithza@gmail.com>
2021-10-09 15:37:10 +0530

    Tickets

--- chat_bot.py
+++ chat_bot.py
@@ -1,6 +1,11 @@
 import time
 
 continue_flag = 1
+tickets = ['123','124']
+ticket_status = {
+    '123':'res',
+    '1234':'not_res'
+}
 
 while (continue_flag==1):
     current_time = time.strftime("%H", time.localtime())
@@ -11,12 +16,37 @@ while (continue_flag==1):
     else:
         print('Good Night')
 
-    print('Do you want to continue? Y/N')
+    print('Please enter your name')
+    user_name = input()
+    print(user_name+', I hope you\'re doing well')
+
+    print('Do you have a ticket number Y/N')
+    ticket_choice = input()
+    if(ticket_choice=='Y' or ticket_choice=='y'):
+        ticket_flag = 1
+        while(ticket_flag==1):
+            print('Please enter your ticket number')
+            ticket_number = input()
+            try:
+                if(ticket_status[ticket_number]):
+                    ticket_flag=0
+                    print('Ticket number found')
+            except Exception:
+                print('Ticket number not found, please enter again')
+                
+    else:
+        print('Create new ticket number')
+    
+
+
+
+
+    print('Do you want to continue? Y/N \n\n\n')
     end_choice = input()
     if(end_choice=='y' or end_choice=='Y'):
         continue_flag = 1
     else:
         continue_flag = 0
-        print('Thank you!')
+        print('Thank you!\n\n\n')
     
 
